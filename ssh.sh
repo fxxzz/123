f26c6eb855025393b710e35582fcdda127a5f1eb
fxxzz <59539542+fxxzz@users.noreply.github.com>
2026-09-24 17:03:01 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -8,7 +8,7 @@ mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 
 
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIN4uOC31nqauqW85lC1B4jnO4HGmGxrJC+4r7vMBzb2" > /root/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILrIoLqJzTVKV5G+P7BPesXX21cJjRu9CrUDl3xNEvO/" > /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 
 
